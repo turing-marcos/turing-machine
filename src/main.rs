7ce88bd1b5df2a545d2a527900aae4567010f097
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2022-11-22 00:17:34 +0100

    Necessary files for wasm

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,26 @@ pub struct Cli {
     cli: bool,
 }
 
+// when compiling to web using trunk.
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Make sure panics are logged using `console.error`.
+    console_error_panic_hook::set_once();
+
+    // Redirect tracing to console.log and friends:
+    tracing_wasm::set_as_global_default();
+
+    let web_options = eframe::WebOptions::default();
+    eframe::start_web(
+        "the_canvas_id", // hardcode it
+        web_options,
+        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+    )
+    .expect("failed to start eframe");
+}
+
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     let args = Cli::parse();
 
