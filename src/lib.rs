96d1245b40b00e00280052a2caf2d885ded39c65
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2022-11-23 13:43:02 +0100

    Improved tests

--- src/lib.rs
+++ src/lib.rs
@@ -15,48 +15,81 @@ mod tests {
     use crate::Rule;
     use crate::TuringMachine;
     use crate::TuringParser;
-    use pest::Parser;
+    use pest::{consumes_to, parses_to};
 
     #[test]
     fn parse_description() {
         let test = "/// a + b\r\n";
-        let mut res = match TuringParser::parse(Rule::description, test) {
-            Ok(v) => v,
-            Err(e) => panic!("Parsing error: {}", e),
-        };
-        assert_eq!(res.next().unwrap().as_span().as_str(), test)
+
+        parses_to! {
+            parser: TuringParser,
+            input: test,
+            rule: Rule::description,
+            tokens: [
+                description(0, 11),
+            ]
+        }
     }
 
     #[test]
     fn parse_tape() {
-        let test = "{[q0]11111011};";
-        let mut res = match TuringParser::parse(Rule::tape, test) {
-            Ok(v) => v,
-            Err(e) => panic!("Parsing error: {}", e),
-        };
-        assert_eq!(res.next().unwrap().as_span().as_str(), test)
+        let test = "{[q0]111011};";
+        
+        parses_to! {
+            parser: TuringParser,
+            input: test,
+            rule: Rule::tape,
+            tokens: [
+                tape(0, 13, [
+                    initial_state(1, 5, [
+                        state(2, 4)
+                    ]),
+                    value(5, 6),
+                    value(6, 7),
+                    value(7, 8),
+                    value(8, 9),
+                    value(9, 10),
+                    value(10, 11),
+                ]),
+            ]
+        }
     }
 
     #[test]
     fn parse_final_state() {
         let test = "F = {q2};";
 
-        let mut res = match TuringParser::parse(Rule::final_state, test) {
-            Ok(v) => v,
-            Err(e) => panic!("Parsing error: {}", e),
-        };
-        assert_eq!(res.next().unwrap().as_span().as_str(), test)
+        
+        parses_to! {
+            parser: TuringParser,
+            input: test,
+            rule: Rule::final_state,
+            tokens: [
+                final_state(0, 9, [
+                    state(5, 7)
+                ])
+            ]
+        }
     }
 
     #[test]
     fn parse_instruction() {
         let test = "(q0, 1, 0, R, q1);";
-
-        let mut res = match TuringParser::parse(Rule::instruction, test) {
-            Ok(v) => v,
-            Err(e) => panic!("Parsing error: {}", e),
-        };
-        assert_eq!(res.next().unwrap().as_span().as_str(), test)
+        
+        parses_to! {
+            parser: TuringParser,
+            input: test,
+            rule: Rule::instruction,
+            tokens: [
+                instruction(0, 18, [
+                    state(1, 3),
+                    value(5, 6),
+                    value(8, 9),
+                    movement(11, 12),
+                    state(14, 16)
+                ]),
+            ]
+        }
     }
 
     #[test]
