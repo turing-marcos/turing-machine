a1078253a6899dbdc8580131820f782a47682125
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2023-03-27 22:43:36 +0200

    Improved workbook

--- src/windows/book.rs
+++ src/windows/book.rs
@@ -20,31 +20,41 @@ impl Exercise {
 
 pub struct BookWindow {
     lang: String,
-    exercises: Vec<Exercise>,
-    selected: usize,
+    exercises: Vec<(String, Vec<Exercise>)>,
+    selected: (usize, usize),
 }
 
 impl BookWindow {
     pub fn new(lang: &str) -> Self {
-        let exercises = vec![
-            Exercise::new(
-                "Exercise 1",
-                include_bytes!("../../assets/ui/exercise1/cover.png"),
-                String::from_utf8(include_bytes!("../../assets/ui/exercise1/code.tm").to_vec())
-                    .unwrap(),
+        let exercises: Vec<(String, Vec<Exercise>)> = vec![
+            (
+                "Chapter 1".to_string(),
+                vec![
+                    Exercise::new(
+                        "Exercise 1",
+                        include_bytes!("../../assets/ui/exercise1/cover.png"),
+                        String::from_utf8(
+                            include_bytes!("../../assets/ui/exercise1/code.tm").to_vec(),
+                        )
+                        .unwrap(),
+                    ),
+                    Exercise::new(
+                        "Exercise 2",
+                        include_bytes!("../../assets/ui/exercise2/cover.png"),
+                        String::from_utf8(
+                            include_bytes!("../../assets/ui/exercise2/code.tm").to_vec(),
+                        )
+                        .unwrap(),
+                    ),
+                ],
             ),
-            Exercise::new(
-                "Exercise 2",
-                include_bytes!("../../assets/ui/exercise2/cover.png"),
-                String::from_utf8(include_bytes!("../../assets/ui/exercise2/code.tm").to_vec())
-                    .unwrap(),
-            ),
+            ("Chapter 2".to_string(), vec![]),
         ];
 
         Self {
             lang: String::from(lang),
             exercises,
-            selected: 0,
+            selected: (0, 0),
         }
     }
 
@@ -61,18 +71,26 @@ impl BookWindow {
             .resizable(false)
             .open(&mut active)
             .show(ctx, |ui| {
-                ui.horizontal(|ui|{
+                ui.horizontal_centered(|ui| {
                     ui.vertical(|ui| {
                         ui.heading("Catalog"); //t!("title.exercises", self.lang));
 
                         egui::ScrollArea::vertical().show(ui, |ui| {
-                            for (i, exercise) in self.exercises.iter().enumerate() {
-                                if ui
-                                    .add_enabled(self.selected != i, egui::Button::new(&exercise.title))
-                                    .clicked()
-                                {
-                                    self.selected = i;
-                                }
+                            for (section, (title, exercises)) in self.exercises.iter().enumerate() {
+                                ui.collapsing(title, |ui| {
+                                    for (i, exercise) in exercises.iter().enumerate() {
+                                        if ui
+                                            .add_enabled(
+                                                self.selected.0 != section || self.selected.1 != i,
+                                                egui::Button::new(&exercise.title),
+                                            )
+                                            .clicked()
+                                        {
+                                            self.selected.0 = section;
+                                            self.selected.1 = i;
+                                        }
+                                    }
+                                });
                             }
                         });
                     });
@@ -83,38 +101,42 @@ impl BookWindow {
                     });
 
                     ui.vertical_centered_justified(|ui| {
-                        self.exercises[self.selected]
+                        self.get_exercise(self.selected)
                             .image
                             .show_max_size(ui, Vec2::new(600.0, 500.0));
 
                         ui.horizontal(|ui| {
                             if ui
-                                .add_enabled(self.selected > 0, egui::Button::new("Previous"))
+                                .add_enabled(self.selected.1 > 0, egui::Button::new("Previous"))
                                 .clicked()
                             {
-                                self.selected -= 1;
+                                self.selected.1 -= 1;
                             }
 
                             ui.add_space(ui.available_width() - 50.0);
 
                             if ui
                                 .add_enabled(
-                                    self.selected < self.exercises.len() - 1,
+                                    self.selected.1 < self.exercises[self.selected.0].1.len() - 1,
                                     egui::Button::new("Next"),
                                 )
                                 .clicked()
                             {
-                                self.selected += 1;
+                                self.selected.1 += 1;
                             }
                         });
 
                         if ui.button("Use this exercise").clicked() {
-                            code = Some(self.exercises[self.selected].code.clone());
+                            code = Some(self.get_exercise(self.selected).code.clone());
                         }
                     });
                 });
             });
 
         (active, code)
+    }
+
+    fn get_exercise(&self, i: (usize, usize)) -> &Exercise {
+        &self.exercises[i.0].1[i.1]
     }
 }
