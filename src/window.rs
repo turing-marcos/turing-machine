22b9e04167978d2f8a74ff37da5cb31ed0f9ff76
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2023-04-02 20:55:12 +0200

    Added a save option for the editor

--- src/window.rs
+++ src/window.rs
@@ -1,3 +1,10 @@
+use std::{
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+    time::{Duration, Instant},
+};
+
 use crate::{
     windows::{AboutWindow, BookWindow, DebugWindow, InfiniteLoopWindow, SecondaryWindow},
     TuringWidget,
@@ -6,9 +13,11 @@ use eframe;
 use eframe::egui::{self, Id, RichText, TextEdit, Ui};
 use eframe::epaint::Color32;
 use internationalization::t;
-use log::warn;
+use log::{debug, error, info, trace, warn};
 use turing_lib::TuringMachine;
 use turing_lib::{Rule, TuringOutput};
+
+const DEFAULT_CODE: &str = include_str!("../Examples/Example1.tm");
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Language {
@@ -25,18 +34,45 @@ pub struct MyApp {
     infinite_loop_window: Option<Box<InfiniteLoopWindow>>,
     book_window: Option<Box<BookWindow>>,
     lang: Language,
+
+    file: Option<PathBuf>,
+    autosave: bool,
+    saved_feedback: Option<Instant>,
 }
 
 impl MyApp {
-    pub fn new(tm: TuringMachine, cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(
+        file: &Option<PathBuf>,
+        cc: &eframe::CreationContext<'_>,
+    ) -> Result<Self, pest::error::Error<Rule>> {
+        let code = match file {
+            Some(ref f) => {
+                trace!("File provided: {:?}", file);
+                let unparsed_file = fs::read_to_string(&f).expect("cannot read file");
+                unparsed_file
+            }
+            None => {
+                trace!("No file provided, opening an example");
+                DEFAULT_CODE.to_string()
+            }
+        };
+
+        let tm = match TuringMachine::new(&code) {
+            Ok(t) => t,
+            Err(e) => {
+                return Err(e);
+            }
+        };
+
         let mut st = (*egui::Context::default().style()).clone();
         st.override_font_id = Some(egui::FontId::monospace(14.0));
         st.spacing.slider_width = 250.0;
         st.spacing.button_padding = egui::Vec2::new(10.0, 5.0);
         st.spacing.item_spacing = egui::Vec2::new(10.0, 10.0);
         cc.egui_ctx.set_style(st);
+        cc.egui_ctx.set_debug_on_hover(true);
 
-        Self {
+        Ok(Self {
             code: String::from(&tm.code),
             error: None,
             tm: TuringWidget::new(tm),
@@ -45,7 +81,11 @@ impl MyApp {
             infinite_loop_window: None,
             book_window: None,
             lang: Language::English,
-        }
+
+            file: file.clone(),
+            autosave: file.is_some(),
+            saved_feedback: None,
+        })
     }
 
     pub fn get_lang(&self) -> String {
@@ -166,6 +206,80 @@ impl MyApp {
         };
         self.code = String::from(code);
     }
+
+    /// Saves the current code to the file,
+    /// and returns a time if the file was saved, or None if it wasn't
+    fn save_file(&self) -> Option<Instant> {
+        if let Some(file) = &self.file {
+            if let Ok(mut file) = File::create(file) {
+                if let Err(e) = file.write_all(self.code.as_bytes()) {
+                    error!("Error saving file: {}", e);
+                } else {
+                    info!("File saved");
+                    return Some(Instant::now());
+                }
+            } else {
+                error!("Error opening file \"{}\" for writing", file.display());
+            }
+        }
+
+        None
+    }
+
+    fn draw_saved_feedback_popup(&mut self, ui: &mut egui::Ui, ctx: &egui::Context) {
+        if let Some(start_time) = self.saved_feedback {
+            let elapsed = start_time.elapsed().as_secs_f32();
+
+            // Set the duration of the fade-in and fade-out animations.
+            let fade_in_duration = 0.25;
+            let fade_out_duration = 1.0;
+            let visible_duration = 1.0;
+            let total_duration = fade_in_duration + fade_out_duration + visible_duration;
+
+            if elapsed < total_duration {
+                let alpha = if elapsed < fade_in_duration {
+                    (elapsed / fade_in_duration).clamp(0.0, 1.0)
+                } else if elapsed > fade_in_duration + visible_duration {
+                    ((total_duration - elapsed) / fade_in_duration).clamp(0.0, 1.0)
+                } else {
+                    1.0
+                };
+
+                let color = Color32::from_white_alpha((alpha * 255.0) as u8);
+
+                // Modify cursor position to make the popup appear above and on top
+                let cursor_pos = ui.cursor();
+                let popup_size = egui::Vec2::new(100.0, 30.0);
+                let popup_pos: egui::Pos2;
+
+                if ui.available_height() == 0.0 {
+                    popup_pos = egui::Pos2::new(
+                        cursor_pos.center().x - popup_size.x / 2.0,
+                        cursor_pos.top() - 200.0,
+                    );
+                } else {
+                    popup_pos = egui::Pos2::new(
+                        cursor_pos.center().x - popup_size.x / 2.0,
+                        cursor_pos.top(),
+                    );
+                }
+
+                let rect = egui::Rect::from_min_size(popup_pos, popup_size);
+                ui.allocate_space(rect.size());
+                let mut popup_ui = ui.child_ui(rect, egui::Layout::default());
+
+                egui::Frame::popup(&ctx.style())
+                    .fill(Color32::from_rgba_unmultiplied(0, 0, 0, 150))
+                    .show(&mut popup_ui, |ui| {
+                        ui.label(egui::RichText::new("Saved file").color(color));
+                    });
+
+                ctx.request_repaint_after(Duration::from_millis(50));
+            } else {
+                self.saved_feedback = None;
+            }
+        }
+    }
 }
 
 impl eframe::App for MyApp {
@@ -223,6 +337,16 @@ impl eframe::App for MyApp {
             .default_height(35.0)
             .show(ctx, |ui| {
                 ui.horizontal_centered(|ui| {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Open").clicked() {}
+
+                        if ui.button("Save").clicked() {}
+
+                        if ui.button("Save as...").clicked() {}
+
+                        ui.checkbox(&mut self.autosave, "Autosave");
+                    });
+
                     ui.menu_button(t!("menu.debugger", lang), |ui| {
                         let mut debug_enabled = self.debug_window.is_some();
                         ui.checkbox(&mut debug_enabled, t!("menu.debugger.activate", lang));
@@ -268,71 +392,127 @@ impl eframe::App for MyApp {
         self.tm.left = egui::SidePanel::left("left")
             .show(ctx, |ui| {
                 ui.vertical_centered_justified(|ui| {
-                    // if ui.button(t!("btn.open_file", self.lang)).clicked() {
-                    //     if cfg!(wasm) {
-                    //         // Spawn dialog on main thread
-                    //         let task = rfd::AsyncFileDialog::new().pick_file();
+                    ui.add_space(10.0);
 
-                    //         // Await somewhere else
-                    //         wasm_bindgen_futures::spawn_local(async move {
-                    //             let file = task.await;
+                    ui.horizontal(|ui| {
+                        let spacer = 10.0;
 
-                    //             if let Some(file) = file {
-                    //                 // If you care about wasm support you just read() the file
-                    //                 let buffer = file.read().await;
-                    //                 match String::from_utf8(buffer) {
-                    //                     Ok(s) => self.restart(&s),
-                    //                     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-                    //                 }
-                    //             }
-                    //         });
-                    //     } else {
-                    //         let path = std::env::current_dir().unwrap();
+                        if ui
+                            .add(egui::Button::new(t!("btn.open_file", lang)).min_size(
+                                ui.available_size() / 2.0 - egui::Vec2::new(spacer / 2.0, 0.0),
+                            ))
+                            .clicked()
+                        {
+                            #[cfg(target_family = "wasm")]
+                            {
+                                // Spawn dialog on main thread
+                                let task = rfd::AsyncFileDialog::new().pick_file();
 
-                    //         let res = rfd::FileDialog::new()
-                    //             .add_filter("TuringMachine", &["tm"])
-                    //             .set_directory(&path)
-                    //             .pick_files();
+                                // Await somewhere else
+                                wasm_bindgen_futures::spawn_local(async move {
+                                    let file = task.await;
 
-                    //         match res {
-                    //             Some(file) => {
-                    //                 let unparsed_file = std::fs::read_to_string(&file[0])
-                    //                     .expect("cannot read file");
-                    //                 self.restart(&unparsed_file);
-                    //             }
-                    //             None => {}
-                    //         }
-                    //     }
-                    // }
-
-                    #[cfg(not(target_family = "wasm"))]
-                    if !cfg!(wasm) && ui.button("Open file").clicked() {
-                        let path = std::env::current_dir().unwrap();
-
-                        let res = rfd::FileDialog::new()
-                            .add_filter("TuringMachine", &["tm"])
-                            .set_directory(&path)
-                            .pick_files();
-
-                        match res {
-                            Some(file) => {
-                                let unparsed_file =
-                                    std::fs::read_to_string(&file[0]).expect("cannot read file");
-                                self.tm = match self.tm.restart(&unparsed_file) {
-                                    Ok(t) => {
-                                        self.error = None;
-                                        t
+                                    if let Some(file) = file {
+                                        // If you care about wasm support you just read() the file
+                                        let buffer = file.read().await;
+                                        match String::from_utf8(buffer) {
+                                            Ok(s) => {
+                                                self.tm = match self.tm.restart(&s) {
+                                                    Ok(t) => {
+                                                        self.error = None;
+                                                        t
+                                                    }
+                                                    Err(e) => {
+                                                        self.error = Some(e);
+                                                        self.tm.clone()
+                                                    }
+                                                };
+                                                self.code = s;
+                                            }
+                                            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+                                        }
                                     }
-                                    Err(e) => {
-                                        self.error = Some(e);
-                                        self.tm.clone()
+                                });
+                            }
+
+                            #[cfg(not(target_family = "wasm"))]
+                            {
+                                let path = std::env::current_dir().unwrap();
+
+                                let res = rfd::FileDialog::new()
+                                    .add_filter("TuringMachine", &["tm"])
+                                    .set_directory(&path)
+                                    .pick_files();
+
+                                match res {
+                                    Some(file) => {
+                                        let unparsed_file = std::fs::read_to_string(&file[0])
+                                            .expect("cannot read file");
+                                        self.tm = match self.tm.restart(&unparsed_file) {
+                                            Ok(t) => {
+                                                self.error = None;
+                                                t
+                                            }
+                                            Err(e) => {
+                                                self.error = Some(e);
+                                                self.tm.clone()
+                                            }
+                                        };
+                                        self.code = unparsed_file;
+                                    }
+                                    None => {}
+                                }
+                            }
+                        }
+
+                        ui.add_space(spacer);
+
+                        if ui
+                            .add(
+                                egui::Button::new(t!("btn.save_file", lang))
+                                    .min_size(ui.available_size()),
+                            )
+                            .clicked()
+                        {
+                            #[cfg(target_family = "wasm")]
+                            {
+                                // Spawn dialog on main thread
+                                let task = rfd::AsyncFileDialog::new().save_file();
+
+                                // Await somewhere else
+                                wasm_bindgen_futures::spawn_local(async move {
+                                    let file = task.await;
+
+                                    if let Some(file) = file {
+                                        // If you care about wasm support you just read() the file
+                                        file.write_all(self.code.as_bytes()).await;
+                                    }
+                                });
+                            }
+
+                            #[cfg(not(target_family = "wasm"))]
+                            {
+                                let file: Option<PathBuf> = match &self.file {
+                                    Some(f) => Some(f.clone()),
+                                    None => {
+                                        let path = std::env::current_dir().unwrap();
+
+                                        rfd::FileDialog::new()
+                                            .add_filter("TuringMachine", &["tm"])
+                                            .set_directory(&path)
+                                            .save_file()
                                     }
                                 };
-                                self.code = unparsed_file;
+
+                                if let Some(f) = file {
+                                    std::fs::write(&f, self.code.as_bytes())
+                                        .expect("cannot write file");
+                                } else {
+                                    error!("Cannot save file");
+                                }
                             }
-                            None => {}
                         }
-                    }
+                    });
 
                     if ui.button(t!("btn.compile", lang)).clicked() {
                         self.tm = match self.tm.restart(&self.code) {
@@ -351,8 +531,21 @@ impl eframe::App for MyApp {
                         let editor = TextEdit::multiline(&mut self.code)
                             .code_editor()
                             .desired_width(0.0);
-                        editor_focused = my_ui.add(editor).has_focus();
+
+                        let res = my_ui.add(editor);
+
+                        if res.lost_focus() {
+                            debug!("Saving file");
+                            self.saved_feedback = self.save_file();
+                        }
+
+                        editor_focused = res.has_focus();
                     });
+
+                    if self.saved_feedback.is_some() {
+                        debug!("Drawing saved feedback popup");
+                        self.draw_saved_feedback_popup(ui, ctx);
+                    }
                 })
             })
             .response
